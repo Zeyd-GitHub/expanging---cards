625c56ac11e418caab7d26f809c9544a69afe859
zeyd <1zeydinik@gmail/com>
2022-10-29 16:41:52 +0200

    the project is finished

---
+++ scrpt.js
@@ -0,0 +1,14 @@
+const boxes = document.querySelectorAll('.box');
+
+boxes.forEach((box) => {
+  box.addEventListener('click', () => {
+    removeActiveClasses();
+    box.classList.add('active');
+  });
+});
+
+function removeActiveClasses() {
+  boxes.forEach((box) => {
+    box.classList.remove('active');
+  });
+}
